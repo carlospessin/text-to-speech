829acb78d16a383fa9e4ac3e1491712536e1ded0
pessin.ingadigital <carlos@ingadigital.com.br>
2021-09-16 11:53:10 -0300

    first commit

---
+++ tts.js
@@ -0,0 +1,58 @@
+let speech = new SpeechSynthesisUtterance();
+speech.lang = "pt-BR";
+
+let voices = [];
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[16];
+    // [0] - Daniel
+    // [1] - Maria
+    // [16] - Google
+};
+
+// PLAY
+document.querySelector("#play").addEventListener("click", () => {
+    play = document.querySelector("#play")
+    pause = document.querySelector("#pause")
+
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+
+    play.classList.add('hidden');
+    pause.classList.remove('hidden');
+});
+
+// PAUSE
+document.querySelector("#pause").addEventListener("click", () => {
+    pause = document.querySelector("#pause")
+    resume = document.querySelector("#resume")
+
+    window.speechSynthesis.pause();
+
+    pause.classList.add('hidden');
+    resume.classList.remove('hidden');
+});
+
+// RESUME
+document.querySelector("#resume").addEventListener("click", () => {
+    pause = document.querySelector("#pause")
+    resume = document.querySelector("#resume")
+
+    window.speechSynthesis.resume();
+
+    resume.classList.add('hidden');
+    pause.classList.remove('hidden');
+});
+
+// STOP
+document.querySelector("#stop").addEventListener("click", () => {
+    play = document.querySelector("#play")
+    pause = document.querySelector("#pause")
+    resume = document.querySelector("#resume")
+
+    window.speechSynthesis.cancel();
+
+    pause.classList.add('hidden');
+    resume.classList.add('hidden');
+    play.classList.remove('hidden');
+});
